a9330679e128e2f884981f352f5a32b1f710fce9
etawada <tawada@itrade.co.jp>
2016-08-19 15:32:18 +0900

    Add files via upload

---
+++ SoftLayer/prospect_Prov.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+
+#IPアドレスの設定
+ETH00=/etc/sysconfig/network-scripts/ifcfg-eth0:0
+cat > ${ETH00} << ETX
+BOOTPROTO=static
+ONBOOT=yes
+IPADDR=10.132.66.163
+echo "NETMASK=255.255.255.240 
+ETX
+
+service network restart
+
+#時刻修正
+mv /etc/localtime /etc/localtime.org
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+cp /etc/sysconfig/clock /etc/sysconfig/clock.org
+echo 'ZONE="Asia/Tokyo"'  > /etc/sysconfig/clock
