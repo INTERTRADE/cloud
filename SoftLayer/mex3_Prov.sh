3b9039ad4ce809b44203e3b14ae969528ec71e94
etawada <tawada@itrade.co.jp>
2016-08-24 10:36:16 +0900

    Add files via upload

--- SoftLayer/mex3_Prov.sh
+++ SoftLayer/mex3_Prov.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
+# インターフェース無効化
+sed -i -e "s/ONBOOT=yes/ONBOOT=no/g"  /etc/sysconfig/network-scripts/ifcfg-eth1
 
-#IPアドレスの設定
+# IPアドレスの設定
 ETH00=/etc/sysconfig/network-scripts/ifcfg-eth0:0
 cat > ${ETH00} << ETX
 BOOTPROTO=static
@@ -10,10 +12,30 @@ IPADDR=10.132.66.162
 echo "NETMASK=255.255.255.240 
 ETX
 
+# マルチキャストルーティング追加
+echo "any net 239.0.0.0/8 dev eth0" >> /etc/sysconfig/static-routes
+
+# NWサービス再起動
 service network restart
 
-#時刻修正
+# 時刻修正
 mv /etc/localtime /etc/localtime.org
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 cp /etc/sysconfig/clock /etc/sysconfig/clock.org
 echo 'ZONE="Asia/Tokyo"'  > /etc/sysconfig/clock
+
+# カーネルパラメータの変更
+cp -p /etc/sysctl.conf /etc/sysctl.conf.org
+cat >> ${ETH00} << ETX
+kernel.sem = 250 32000 128 1000
+net.core.rmem_default = 1048756
+net.core.wmem_default = 262144
+kernel.shmmni = 4096
+net.core.rmem_max = 33554432
+net.core.wmem_max = 1048576
+net.ipv4.ip_local_port_range = 30001 65500
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_fin_timeout = 30
+net.core.netdev_max_backlog = 2000
+ETX
+
--- SoftLayer/prospect_Prov.sh
+++ SoftLayer/prospect_Prov.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
+# インターフェース無効化
+sed -i -e "s/ONBOOT=yes/ONBOOT=no/g"  /etc/sysconfig/network-scripts/ifcfg-eth1
 
-#IPアドレスの設定
+# IPアドレスの設定
 ETH00=/etc/sysconfig/network-scripts/ifcfg-eth0:0
 cat > ${ETH00} << ETX
 BOOTPROTO=static
@@ -10,9 +12,10 @@ IPADDR=10.132.66.163
 echo "NETMASK=255.255.255.240 
 ETX
 
+# NWサービス再起動
 service network restart
 
-#時刻修正
+# 時刻修正
 mv /etc/localtime /etc/localtime.org
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 cp /etc/sysconfig/clock /etc/sysconfig/clock.org
